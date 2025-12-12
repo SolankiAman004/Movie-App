6548a8c6fad43dc5291a58476cbc9a5d5b4a24a2
SolankiAman004 <amansolanki.ic@gmail.com>
2025-12-12 17:33:24 +0530

    cra changes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (apiPath, queryTerm="") => {
-    const [data, setData] = useState([]);
-    const apiKey = import.meta.env.VITE_APP_API_KEY;
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${apiKey}&query=${encodeURIComponent(queryTerm)}`;
+export const useFetch = (apiPath, queryTerm = "") => {
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        async function fetchMovies(){
-          const response = await fetch(url);
-          const json = await response.json();
-          setData(json.results);
-        }
-        fetchMovies();
-      }, [url])
+  const apiKey = process.env.REACT_APP_TMDB_API_KEY;
+  console.log("API KEY:", apiKey); // should print the key
 
-  return { data }
-}
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${apiKey}&query=${queryTerm}`;
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json.results);
+    }
+    fetchMovies();
+  }, [url]);
+
+  return { data };
+};
